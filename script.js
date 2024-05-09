c633d19c8060b9948304bfc4a2e4d510c6ad6923
winningshop <153954180+winningshop@users.noreply.github.com>
2024-05-09 15:39:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const navVector = document.querySelector('.nav-vector');
 
 const urlDiscord = 'https://discord.gg/nyEZmr63vt';
 const urlDoc = 'https://docs.codingbots.com.br';
+const urlInsta = 'https://instagram.com/codingbotsbr';
+const urlYouTube = 'https://www.youtube.com/@CodingBots';
 
 function categorias() {
     const links = {
@@ -24,116 +26,15 @@ function categorias() {
     });
 }
 
-function adicionarOuvintesRedesSociais() {
-    const redesSociais = [
-        { icon: document.querySelector('.social-icon:nth-child(1)'), link: urlDiscord },
-        { icon: document.querySelector('.social-icon:nth-child(2)'), link: 'https://www.instagram.com/codingbotsbr/' }
-    ];
-
-    redesSociais.forEach(rede => {
-        if (rede.icon) {
-            const linkElement = document.createElement('a');
-            linkElement.href = rede.link;
-            linkElement.target = '_blank';
-            rede.icon.appendChild(linkElement);
-
-            rede.icon.addEventListener('click', () => {
-                window.open(rede.link, '_blank');
-            });
-        }
+function scrollToTarget() {
+    event.preventDefault();
+    var targetElement = document.getElementById('service-title');
+    var targetPosition = targetElement.getBoundingClientRect().top;
+    var offset = -75;
+    window.scrollTo({
+        top: targetPosition + window.pageYOffset + offset,
+        behavior: 'smooth'
     });
-}
-
-function bemVindo() {
-    if (welcomeContainer) {
-        const messages = [
-            'Aqui você tem a automação de servidor',
-            'Discord com integração perfeita ao FiveM.'
-        ];
-
-        const arrow = document.createElement('div');
-        arrow.classList.add('arrow');
-        arrow.innerHTML = '&#9660;';
-
-        const elements = [
-            { tag: 'img', src: 'images/robocoding.png', alt: 'Robô CodingBots', className: 'welcome-image' },
-            { tag: 'h1', textContent: 'Seja bem-vindo à', className: 'welcome-greeting' },
-            { tag: 'h1', textContent: 'CodingBots', className: 'welcome-title' },
-            { tag: 'h1', textContent: messages[0], className: 'welcome-message' },
-            { tag: 'h1', textContent: messages[1], className: 'welcome-message2' },
-            { element: arrow, listener: arrowClickHandler }
-        ];
-
-        elements.forEach(item => {
-            const element = document.createElement(item.tag || item.element.tagName);
-            if (item.tag) {
-                if (item.src) element.src = item.src;
-                if (item.alt) element.alt = item.alt;
-                if (item.textContent) element.textContent = item.textContent;
-                if (item.className) element.classList.add(item.className);
-            } else {
-                if (item.listener) item.element.addEventListener('click', item.listener);
-            }
-            welcomeContainer.appendChild(element);
-        });
-    }
-}
-
-function arrowClickHandler() {
-    const scrollToPoint = document.querySelector('#services-container');
-    if (scrollToPoint) {
-        scrollToPoint.scrollIntoView({ behavior: 'smooth' });
-    }
-}
-
-function createServiceCategory() {
-    const serviceCategory = document.createElement('div');
-    serviceCategory.classList.add('service-category');
-
-    const elements = [
-        { tag: 'div', className: 'service-image', children: [{ tag: 'img', src: 'images/service.png', alt: 'Imagem do Serviço' }] },
-        { tag: 'div', className: 'service-details', children: [
-            { tag: 'h2', textContent: 'O QUE NOSSO BOT OFERECE?', className: 'service-title' },
-            { tag: 'p', innerHTML: 'Oferecemos um conjunto abrangente de <b>soluções automatizadas</b> para <b>servidores</b><br>' +
-                                       '<b>FiveM</b>, incluindo sistemas de whitelist, tickets, transcrições, conectividade,<br>' +
-                                       'anúncios, changelog, gerenciamento de jogadores, registros de entrada/saída e muito<br>' +
-                                       'mais, <b>integrados perfeitamente ao Discord.</b>', className: 'service-description' },
-            { tag: 'a', textContent: 'ASSINAR AGORA', className: 'service-cta' }
-        ]}
-    ];
-
-    elements.forEach(item => {
-        const element = document.createElement(item.tag);
-        if (item.className) element.classList.add(item.className);
-        if (item.textContent) element.textContent = item.textContent;
-        if (item.innerHTML) element.innerHTML = item.innerHTML;
-        if (item.children) {
-            item.children.forEach(child => {
-                const childElement = document.createElement(child.tag);
-                if (child.className) childElement.classList.add(child.className);
-                if (child.textContent) childElement.textContent = child.textContent;
-                if (child.innerHTML) childElement.innerHTML = child.innerHTML;
-                if (child.src) childElement.src = child.src;
-                if (child.alt) childElement.alt = child.alt;
-                element.appendChild(childElement);
-            });
-        }
-        serviceCategory.appendChild(element);
-    });
-
-    return serviceCategory;
-}
-
-function addServiceCategory() {
-    const serviceCategory = createServiceCategory();
-    servicesContainer.appendChild(serviceCategory);
-
-    const buttonBuy = serviceCategory.querySelector('.service-cta');
-    if (buttonBuy) {
-        buttonBuy.addEventListener('click', () => {
-            window.open(urlDiscord, '_blank');
-        });
-    }
 }
 
 function scrollToTop() {
@@ -141,9 +42,6 @@ function scrollToTop() {
 }
 
 function events() {
-    navVector.addEventListener('click', () => {
-        window.open(urlDiscord, '_blank');
-    });
 
     menuNav.addEventListener('click', event => {
         const alvo = event.target;
@@ -163,107 +61,143 @@ function events() {
     });
 }
 
-function simulateDragging() {
-    if (!isDragging) {
-        if (slider.scrollLeft >= slider.scrollWidth - slider.clientWidth) {
-            slider.scrollLeft = 0;
-        } else {
-            slider.scrollLeft += scrollSpeed;
-        }
-    }
-    animationId = requestAnimationFrame(simulateDragging);
-}
-
-window.addEventListener('load', () => {
-    if (!isMobileDevice()) {
-        simulateDragging();
-    }
-});
+let isDragging = false;
+let startPosition = 0;
+let startScrollLeft = 0;
 
 slider.addEventListener('mousedown', (e) => {
     isDragging = true;
-    startPosition = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft;
-    cancelAnimationFrame(animationId);
+    startPosition = e.pageX;
+    startScrollLeft = slider.scrollLeft;
 });
 
-slider.addEventListener('mouseup', () => {
+document.addEventListener('mouseup', () => {
     isDragging = false;
-    if (!isMobileDevice()) {
-        simulateDragging();
-    }
 });
 
 slider.addEventListener('mousemove', (e) => {
     if (!isDragging) return;
-    e.preventDefault();
-    const deltaX = e.pageX - slider.offsetLeft - startPosition;
-    slider.scrollLeft = scrollLeft - deltaX;
+    const deltaX = e.pageX - startPosition;
+    slider.scrollLeft = startScrollLeft - deltaX;
 });
 
-function isMobileDevice() {
-    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-}
+slider.addEventListener('mouseleave', () => {
+    isDragging = false;
+});
 
-function loginButtonClickHandler() {
+
+function loginButtonClickHandler(event) {
+    event.preventDefault();
+
     const overlay = $('#overlay');
     const loginModal = $('#loginModal');
     const usernameInput = $('#username');
     const passwordInput = $('#password');
 
-    overlay.css('display', 'block');
-    loginModal.fadeIn(500).addClass('animate__animated animate__fadeIn');
+    overlay.css('display', 'block').on('click', function(e) {
+        if (e.target === this) {
+            $(this).css('display', 'none');
+            loginModal.removeClass('animate__animated animate__fadeIn');
+            loginModal.css('display', 'none');
+            usernameInput.val('');
+            passwordInput.val('');
+        }
+    });
 
-    $('body').css('overflow', 'hidden');
+    loginModal.css({
+        'display': 'block',
+        'position': 'fixed',
+        'top': '50%',
+        'left': '50%',
+        'transform': 'translate(-50%, -50%)',
+        'z-index': '9999'
+    }).addClass('animate__animated animate__fadeIn');
 
     $('.register').on('click', function(event) {
         event.preventDefault();
-        var url = $(this).attr('href'); // Obtém o URL do atributo href do link
-
-        window.open(url, '_blank'); // Abre o URL em uma nova guia
+        var url = $(this).attr('href');
+        window.open(url, '_blank');
     });
 
-    // Evento de clique para fechar o modal
     $('.close').on('click', function() {
         overlay.css('display', 'none');
         loginModal.removeClass('animate__animated animate__fadeIn');
-        $('body').css('overflow', 'auto');
-
+        loginModal.css('display', 'none');
         usernameInput.val('');
         passwordInput.val('');
     });
+}
 
-    
-    fetch("http://181.215.254.155:3000/api/getUsers")
-    .then((response) => {
-        return response.json(); // Transforma a resposta em JSON
+function loginButtonHandler() {
+    const usernameInput = $('#username');
+    const passwordInput = $('#password');
+    const username = usernameInput.val();
+    const password = passwordInput.val();
+
+    if (!username || !password) {
+        showNotification('Por favor, preencha todos os campos!', false);
+        return;
+    }
+
+    fetch("http://181.215.254.155:3000/api/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: username,
+            password: password
+        })
     })
-    .then((data) => {
-        console.log('Dados recebidos:', data); // Exibe os dados no console
+    .then(async (response) => {
+        const data = await response.json();
+        if (response.ok) {
+            const dados = data.user;
+            console.log(dados);
+            showNotification('Logado com sucesso!', true);
+        } else {
+            console.error('Erro:', data.error);
+            showNotification('Usuário ou senha inválidos!', false);
+        }
     })
-    .catch((error) => {
-        console.error('Erro:', error); // Exibe erros no console
+    .catch(() => {
+        showNotification('Sistema indisponível! Tente novamente mais tarde!', false);
     });
-
-
-    
-
 }
 
 
+function showNotification(message, isSuccess) {
+    const notification = document.getElementById('notification');
+    const iconSuccess = notification.querySelector('.notification-icon.success');
+    const iconError = notification.querySelector('.notification-icon.error');
+    const span = notification.querySelector('span');
+    span.textContent = message;
 
+    if (isSuccess) {
+        iconSuccess.style.display = 'inline-block';
+        iconError.style.display = 'none';
+        notification.classList.remove('error');
+        notification.classList.add('success');
+    } else {
+        iconSuccess.style.display = 'none';
+        iconError.style.display = 'inline-block';
+        notification.classList.remove('success');
+        notification.classList.add('error');
+    }
 
+    notification.style.top = '50px';
 
-
-
+    setTimeout(() => {
+        notification.style.top = '-100px';
+    }, 4000);
+}
 
 window.addEventListener('DOMContentLoaded', () => {
     categorias();
     events();
-    bemVindo();
-    createServiceCategory();
-    addServiceCategory();
-    adicionarOuvintesRedesSociais();
-
     $('.loginButton').on('click', loginButtonClickHandler);
+    $('#loginForm').on('submit', function(event) {
+        loginButtonClickHandler(event);
+        loginButtonHandler();
+    });
 });
